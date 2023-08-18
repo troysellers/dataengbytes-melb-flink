d20420bd1fa021d7d376c1704fc05b7ee15d9e12
Troy Sellers <768991+troysellers@users.noreply.github.com>
2023-08-18 13:34:15 +1000

    adding first flink job

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -65,7 +65,7 @@ resource "local_sensitive_file" "env" {
     content = <<EOF
 PROJECT_NAME="${var.aiven_project_name}"
 SERVICE_NAME="${aiven_kafka.kafka-service.service_name}"
-TOPIC="${var.kafka_topic}"
+TOPIC="${var.kafka_topic_filtering_in}"
 PARTITIONS=2
 REPLICATION=2
 NR_MESSAGES=0
@@ -88,3 +88,76 @@ resource "null_resource" "load_pg_data" {
         command = "psql ${aiven_pg.postgres-service.service_uri} -f ${abspath(path.module)}/../data/create.sql "
     }
 }
+
+
+
+#Flink App 1
+resource "aiven_flink_application" "filtering" {
+    project = var.aiven_project_name
+    service_name = aiven_flink.flink.service_name
+    name = "filtering"
+}
+
+resource "aiven_flink_application_version" "filtering_v1" {
+    project = var.aiven_project_name
+    service_name = aiven_flink.flink.service_name
+    application_id = aiven_flink_application.filtering.application_id
+    statement = <<EOT
+        INSERT INTO CPU_OUT_FILTER 
+        SELECT 
+            time_ltz, 
+            hostname, 
+            cpu, 
+            usage 
+        FROM CPU_IN 
+        WHERE usage > 80
+    EOT
+    sink {
+        create_table = <<EOT
+            CREATE TABLE CPU_OUT_FILTER (
+                time_ltz TIMESTAMP(3),
+                hostname STRING,
+                cpu STRING,
+                usage DOUBLE
+                )
+            WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = '',
+            'topic' = '${var.kafka_topic_filtering_out}',
+            'value.format' = 'json',
+            'scan.startup.mode' = 'earliest-offset'
+            )
+        EOT
+        integration_id = aiven_service_integration.flink_integration_kafka.integration_id
+    }
+    source {
+        create_table = <<EOT
+            CREATE TABLE CPU_IN (
+                hostname STRING,
+                cpu STRING,
+                usage DOUBLE,
+                occurred_at BIGINT,
+                time_ltz AS TO_TIMESTAMP_LTZ(occurred_at, 3),
+                WATERMARK FOR time_ltz AS time_ltz - INTERVAL '10' SECOND
+                )
+            WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = '',
+            'topic' = '${var.kafka_topic_filtering_in}',
+            'value.format' = 'json',
+            'scan.startup.mode' = 'earliest-offset'
+            )
+        EOT
+        integration_id = aiven_service_integration.flink_integration_kafka.integration_id
+    }
+}
+
+# flink job 1 application deployment
+resource "aiven_flink_application_deployment" "filtering_v1_deployment" {
+  application_id = aiven_flink_application.filtering.application_id
+  project = var.aiven_project_name
+  service_name = aiven_flink.flink.service_name
+  version_id = aiven_flink_application_version.filtering_v1.application_version_id
+  parallelism = 1
+}
+
